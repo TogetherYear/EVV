9cdae800bd4512f84b6b25c90c400b3a122957f7
章文俊 <1346502061@qq.com>
2022-11-24 19:52:53 +0800

    Transformable

---
+++ src/render/common/Transformable/Transformable.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { Transformable } from './Transformable';
+
+const instance = new Transformable()
+
+const {
+    dom,
+    UpdateSize
+} = instance.InitStates()
+instance.InitHooks()
+instance.Run()
+
+defineExpose({
+    /**
+     * 如果容器改变大小 调一下这个方法
+     */
+    UpdateSize
+})
+</script>
+
+<template>
+    <div class="Transformable" ref="dom">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./Transformable.scss"
+</style>
