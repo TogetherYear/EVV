dacb91dc80cf3e1013dfacc5bc57ea1c4acab2f0
Together <togetheryear@gmail.com>
2022-12-13 23:42:58 +0800

    public

--- vite.config.ts
+++ vite.config.ts
@@ -6,45 +6,45 @@ import electron from 'vitejs-plugin-electron'
 const root = join(__dirname, 'src/render')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      script: {
-        reactivityTransform: true
-      }
-    }),
-    electron()
-  ],
-  root,
-  base: './', // index.html 中静态资源加载位置
-  envDir: join(__dirname, 'env'),
-  server: {
-    port: 6768 // 开发环境的启动端口
-  },
-  resolve: {
-    alias: {
-      '@render': join(__dirname, 'src/render'),
-      '@main': join(__dirname, 'src/main'),
-      '@libs': join(__dirname, 'src/libs'),
-      '@decorators': join(__dirname, 'src/decorators'),
-      '@src': join(__dirname, 'src'),
-      '@root': __dirname
+    plugins: [
+        vue({
+            script: {
+                reactivityTransform: true
+            }
+        }),
+        electron()
+    ],
+    root,
+    base: './', // index.html 中静态资源加载位置
+    envDir: join(__dirname, 'env'),
+    server: {
+        port: 6768 // 开发环境的启动端口
+    },
+    resolve: {
+        alias: {
+            '@render': join(__dirname, 'src/render'),
+            '@main': join(__dirname, 'src/main'),
+            '@libs': join(__dirname, 'src/libs'),
+            '@decorators': join(__dirname, 'src/decorators'),
+            '@src': join(__dirname, 'src'),
+            '@root': __dirname
+        }
+    },
+    build: {
+        outDir: join(__dirname, 'dist/render'),
+        emptyOutDir: true,
+        // minify: false,
+        // commonjsOptions: {},
+        assetsDir: '', // 相对路径 加载问题
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                format: 'cjs'
+            },
+            external: ['electron']
+        }
+    },
+    optimizeDeps: {
+        exclude: ['electron']
     }
-  },
-  build: {
-    outDir: join(__dirname, 'dist/render'),
-    emptyOutDir: true,
-    // minify: false,
-    // commonjsOptions: {},
-    assetsDir: '', // 相对路径 加载问题
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        format: 'cjs'
-      },
-      external: ['electron']
-    }
-  },
-  optimizeDeps: {
-    exclude: ['electron']
-  }
 })
