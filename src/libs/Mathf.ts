4e958717337e02605382de4744777306e25ba01a
章文俊 <1346502061@qq.com>
2023-07-14 10:24:41 +0800

    Mathf

--- src/libs/Mathf.ts
+++ src/libs/Mathf.ts
@@ -15,6 +15,24 @@ class Mathf {
         return a + (b - a) * weight
     }
 
+    /**
+     * 转化为弧度
+     */
+    static Rad(d: number) {
+        return d * this.Deg2Rad
+    }
+
+    /**
+     * 转化为角度
+     */
+    static Deg(r: number) {
+        return r * this.Rad2Deg
+    }
+
+    static Deg2Rad = 0.017453292;
+
+    static Rad2Deg = 57.29578;
+
 }
 
 export { Mathf }
