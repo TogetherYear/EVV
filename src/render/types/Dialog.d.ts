f2c9de69a9fc831cab2c9eeade2256cf7b397753
Together <togetheryear@gmail.com>
2023-11-27 22:43:59 +0800

    Update:Format

--- src/render/types/Dialog.d.ts
+++ src/render/types/Dialog.d.ts
@@ -1,8 +1,13 @@
 declare namespace Dialog {
     export function destroyAll(): void
+
     export function create(...args: any): void
+
     export function success(...args: any): void
+
     export function warning(...args: any): void
+
     export function error(...args: any): void
+
     export function info(...args: any): void
 }
--- src/render/types/Message.d.ts
+++ src/render/types/Message.d.ts
@@ -1,9 +1,14 @@
 declare namespace Message {
     export function info(content: string, ...args: any): void
+
     export function success(content: string, ...args: any): void
+
     export function warning(content: string, ...args: any): void
+
     export function error(content: string, ...args: any): void
+
     export function loading(content: string, ...args: any): void
+
     export function destroyAll(): void
 
 }
