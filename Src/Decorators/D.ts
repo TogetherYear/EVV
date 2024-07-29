e473aefafc1f0d2088ceed8b187c031a32288741
TSingleton <togetheryear@gmail.com>
2024-07-29 14:21:36 +0800

    Update:Config

--- Src/Decorators/D.ts
+++ Src/Decorators/D.ts
@@ -1,26 +1,4 @@
 namespace D {
-    export function ClassDec() {
-        return function <T extends new (...args: Array<any>) => Object>(C: T) {
-            return class extends C {
-                constructor(...args: Array<any>) {
-                    super(...args);
-                    this.Hooks();
-                }
-
-                private Hooks() {}
-            };
-        };
-    }
-
-    export function FunctionDec() {
-        return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
-            const original = descriptor.value.bind(target);
-            descriptor.value = (...args: Array<unknown>) => {
-                original(...args);
-            };
-        };
-    }
-
     const debounceMap = new Map<string, NodeJS.Timeout>();
 
     const throttleMap = new Map<string, number>();
--- Src/Main/Decorators/DM.ts
+++ Src/Main/Decorators/DM.ts
@@ -1,26 +1,4 @@
 namespace DM {
-    export function ClassDec() {
-        return function <T extends new (...args: Array<any>) => Object>(C: T) {
-            return class extends C {
-                constructor(...args: Array<any>) {
-                    super(...args);
-                    this.Hooks();
-                }
-
-                private Hooks() {}
-            };
-        };
-    }
-
-    export function FunctionDec() {
-        return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
-            const original = descriptor.value.bind(target);
-            descriptor.value = (...args: Array<unknown>) => {
-                original(...args);
-            };
-        };
-    }
-
     export enum ChildrenProcessType {
         Log = 'Log',
         Push = 'Push',
