e768a2efcb493fa952127b7394916511304a6301
TSingleton <togetheryear@gmail.com>
2024-07-29 14:07:14 +0800

    Update:Config

--- vite.config.ts
+++ vite.config.ts
@@ -2,50 +2,55 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { join } from 'path';
 
-export default defineConfig({
-    plugins: [
-        vue({
-            script: {
-                defineModel: true
+export default defineConfig(({ command, mode }) => {
+    return {
+        plugins: [
+            vue({
+                script: {
+                    defineModel: true
+                }
+            })
+        ],
+        root: join(__dirname, 'Src/Render'),
+        base: './',
+        envDir: join(__dirname, 'Env'),
+        server: {
+            port: 6768
+        },
+        publicDir: 'Public',
+        resolve: {
+            alias: {
+                '@Render': join(__dirname, 'Src/Render'),
+                '@Main': join(__dirname, 'Src/Main'),
+                '@Libs': join(__dirname, 'Src/Libs'),
+                '@Decorators': join(__dirname, 'Src/Decorators'),
+                '@Src': join(__dirname, 'Src'),
+                '@Root': __dirname
             }
-        })
-    ],
-    root: join(__dirname, 'Src/Render'),
-    base: './',
-    envDir: join(__dirname, 'Env'),
-    server: {
-        port: 6768
-    },
-    publicDir: 'Public',
-    resolve: {
-        alias: {
-            '@Render': join(__dirname, 'Src/Render'),
-            '@Main': join(__dirname, 'Src/Main'),
-            '@Libs': join(__dirname, 'Src/Libs'),
-            '@Decorators': join(__dirname, 'Src/Decorators'),
-            '@Src': join(__dirname, 'Src'),
-            '@Root': __dirname
-        }
-    },
-    build: {
-        outDir: join(__dirname, 'Dist/Render'),
-        emptyOutDir: true,
-        minify: 'esbuild',
-        assetsDir: 'Source',
-        sourcemap: false,
-        target: 'esnext',
-        rollupOptions: {
-            output: {
-                manualChunks: (id: string) => {
-                    if (id.includes('node_modules')) {
-                        return 'Vendor';
+        },
+        esbuild: {
+            drop: command === 'serve' ? [] : ['console', 'debugger']
+        },
+        build: {
+            outDir: join(__dirname, 'Dist/Render'),
+            emptyOutDir: true,
+            minify: 'esbuild',
+            assetsDir: 'Source',
+            sourcemap: false,
+            target: 'esnext',
+            rollupOptions: {
+                output: {
+                    manualChunks: (id: string) => {
+                        if (id.includes('node_modules')) {
+                            return 'Vendor';
+                        }
                     }
                 }
             }
+        },
+        optimizeDeps: {
+            include: [],
+            exclude: ['electron']
         }
-    },
-    optimizeDeps: {
-        include: [],
-        exclude: ['electron']
-    }
+    };
 });
