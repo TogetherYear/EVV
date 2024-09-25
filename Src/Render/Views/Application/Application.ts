25dfa0ec41e6d8198dd1ede8dc4b70db63ea7d5a
TSingleton <togetheryear@gmail.com>
2024-09-25 13:50:38 +0800

    Update:Resize

--- Src/Render/Views/Application/Application.ts
+++ Src/Render/Views/Application/Application.ts
@@ -3,6 +3,7 @@ import { App } from '@Render/App/App';
 import { onMounted, onUnmounted } from 'vue';
 import { Component } from '@Render/Libs/Component';
 import { TEvent } from '@Render/Decorators/TEvent';
+import { TTool } from '@Render/Decorators/TTool';
 
 class Application extends Component {
     public constructor() {
@@ -16,8 +17,8 @@ class Application extends Component {
     public InitHooks() {}
 
     public Run() {
-        onMounted(() => {
-            this.State();
+        onMounted(async () => {
+            await this.SetDefault();
         });
         onUnmounted(() => {
             this.Destroy();
@@ -26,24 +27,25 @@ class Application extends Component {
 
     protected Destroy() {}
 
+    private async SetDefault() {
+        await Renderer.Widget.SetSize({
+            width: parseInt(localStorage.getItem('Application_Width') || '1000'),
+            height: parseInt(localStorage.getItem('Application_Height') || '560')
+        });
+        await Renderer.Widget.Center();
+        await Renderer.Widget.Show();
+    }
+
     @TEvent.Listen(App, I.IpcRendererEvent.SecondInstance)
     private async OnSecondInstance(e: I.IpcRendererSendMessage) {
         await Renderer.Widget.Show();
     }
 
-    private async State() {
-        if (location.href.indexOf('Application') != -1) {
-            await Renderer.Widget.SetSize({
-                width: parseInt(localStorage.getItem('Application_Width') || '1000'),
-                height: parseInt(localStorage.getItem('Application_Height') || '560')
-            });
-            await Renderer.Widget.Center();
-            await Renderer.Widget.Show();
-            window.addEventListener('resize', (e: UIEvent) => {
-                localStorage.setItem('Application_Width', `${window.innerWidth}`);
-                localStorage.setItem('Application_Height', `${window.innerHeight}`);
-            });
-        }
+    @TTool.Debounce(100)
+    @TEvent.Listen(window, 'resize')
+    private OnResized(e: UIEvent) {
+        localStorage.setItem('Application_Width', `${window.innerWidth}`);
+        localStorage.setItem('Application_Height', `${window.innerHeight}`);
     }
 }
 
