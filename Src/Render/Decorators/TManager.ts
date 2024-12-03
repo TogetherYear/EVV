9e54a3b3ce4e58dbabf26989ebb8a0b1d209d38e
章文俊 <1346502061@qq.com>
2024-12-03 10:15:58 +0800

    Update:Manager

--- Src/Render/Decorators/TManager.ts
+++ Src/Render/Decorators/TManager.ts
@@ -1,28 +1,15 @@
 import { Manager } from '@Render/Libs/Manager';
 
 namespace TManager {
-    export const manager: Array<Manager> = [];
-
     export function Generate() {
         return function <T extends new (...args: Array<any>) => Manager>(C: T) {
             return class extends C {
                 constructor(...args: Array<any>) {
                     super(...args);
                     this.TManager_Generate_Hooks();
-                    this.Mount();
                 }
 
-                private TManager_Generate_Hooks() {
-                    manager.push(this);
-                }
-
-                private Mount() {
-                    //@ts-ignore
-                    if (!window.manager) {
-                        //@ts-ignore
-                        window.manager = manager;
-                    }
-                }
+                private TManager_Generate_Hooks() {}
             };
         };
     }
