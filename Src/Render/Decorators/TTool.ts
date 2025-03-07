852d4dcc553372a61378406137bcdcb9c3ae006d
章文俊 <1346502061@qq.com>
2025-03-07 11:37:55 +0800

    Update:Component

--- Src/Render/Decorators/TTool.ts
+++ Src/Render/Decorators/TTool.ts
@@ -4,6 +4,7 @@ import { TEvent } from './TEvent';
 import { Mathf } from '@Src/Utils/Mathf';
 import { Entity } from '@Render/Libs/Entity';
 import { Time } from '@Src/Utils/Time';
+import { Component } from '@Render/Libs/Component';
 
 namespace TTool {
     /**
@@ -408,7 +409,7 @@ namespace TTool {
     /**
      * 简单缓存页面 ( 此装饰器需要放在最下面 ) 参数为字符串
      */
-    export function Cache<V>(needs: Array<keyof V>) {
+    export function Cache<V extends Component>(needs: Array<keyof V>) {
         return function <T extends new (...args: Array<any>) => Entity>(C: T) {
             return class extends C {
                 constructor(...args: Array<any>) {
